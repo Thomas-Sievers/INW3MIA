a415b68212a914fafdd29bec7d6e11dcdce74c4a
Thomas Sievers <thomas@qpix.com.br>
2024-03-07 16:02:46 -0300

    commit que eu esqueci de fazer

--- desafiosJS/maior/index.js
+++ desafiosJS/maior/index.js
@@ -1,9 +1,9 @@
 const prompt =  require("prompt-sync")();
 
 function ask(){
-    let num1 = prompt("Escolha um número");
-    let num2 = prompt("Escolha outro número");
-    let num3 = prompt("Escolha um terceiro número");
+    let num1 = parseFloat(prompt("Escolha um número: "));
+    let num2 = parseFloat(prompt("Escolha outro número: "));
+    let num3 = parseFloat(prompt("Escolha um terceiro número: "));
 
     return userChoice = {
         number1 : num1,
@@ -12,13 +12,27 @@ function ask(){
     };
 };
 
+function check(number1, number2, number3){
+    if((number1 == number2) || (number1	== number3) || (number2 == number3)){
+        return console.log("Dois ou mais número são iguais.");
+    }
+    if((number1 > number2) && (number1 > number3)){
+        return console.log(`O número ${number1} é o maior dos 3 números escolhidos`);
+    }
+    else if((number2 > number1) && (number2 > number3)){
+        return console.log(`O número ${number2} é o maior dos 3 números escolhidos`);
+    }
+    else if((number3 > number1) && (number3 > number2)){
+        return console.log(`O número ${number3} é o maior dos 3 números escolhidos`);
+    };
+};
+
 function main(){
     let userData = ask();
     let number1 = userData.number1;
     let number2 = userData.number2;
     let number3 = userData.number3;
-
-    function check(number1, number2, number3){
-        
-    };
+    check(number1, number2, number3);
 };
+
+main();
