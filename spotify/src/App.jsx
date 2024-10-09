64d2a7b478d1e2ed2c42947d9a65bc71a8b7e39a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 08:26:40 -0300

    aprendendo a mapear e a usar useEffect

--- spotify/src/App.jsx
+++ spotify/src/App.jsx
@@ -3,8 +3,20 @@ import SideBar from "./components/SideBar"
 import MainContent from "./components/MainContent"
 import Container from "./components/Container"
 import CardSideBar from "./components/CardSideBar"
+import { useEffect, useState } from "react"
 
 function App() {
+
+  const [ artistas, setArtistas ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/artistas')
+      .then(res => res.json())
+      .then(data => setArtistas(data))
+      .catch(err => console.log(err))
+  }, [])
+
 
   return (
     <>
@@ -15,7 +27,19 @@ function App() {
           <CardSideBar/>
           <CardSideBar/>
         </SideBar>
-        <MainContent/>
+        <MainContent>
+
+          {
+            artistas.map( artistas => (           
+            <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+              <p>{artistas.name}</p>
+              <div className="bg-green-400 w-3/4 h-7"></div>
+            </div>
+            ))
+          }
+
+
+        </MainContent>
       </Container>
     </>
   )
